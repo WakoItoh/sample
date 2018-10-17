019d9374b827078926db5373e90bcb33db73efb2
tempadmin <tempadmin@A062941672.rfg.rfgad.ricoh.com>
2018-10-17 17:15:53 +0900

    2nd commit.

--- src/servlet/ShowMessageServlet.java
+++ src/servlet/ShowMessageServlet.java
@@ -19,7 +19,7 @@ public class ShowMessageServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Updated.");
 	}
 
 	/**
